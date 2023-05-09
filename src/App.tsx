9dddaefe0a176e48dea22e37da56f3185bbe3f9f
kylemcshea <kmcshea@stevens.edu>
2023-05-09 15:45:08 -0400

    initialize react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline text-center">
-      Hello world!
-    </h1>
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl text-red-500 font-bold underline text-center">
+        Hello world!
+      </h1>
+    </QueryClientProvider>
   );
 }
 
