5163cfca3c08f5e27be1dafecc623675ee1680e5
kylemcshea <kmcshea@stevens.edu>
2023-05-10 14:18:24 -0400

    add comment pagination endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,23 @@
-import { CommentFetchResponse } from "../components/CommentsFeed";
 import { CreateCommentFormInputs } from "../components/CreateComment";
-import { CREATE_COMMENT, GET_COMMENTS } from "./routes";
+import { CommentFetchResponse } from "../hooks/useCommentsFeed";
+import {
+  CREATE_COMMENT,
+  GET_COMMENTS,
+  GET_COMMENTS_PAGINATION,
+} from "./routes";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
 export const getComments = (): Promise<CommentFetchResponse> =>
   axios.get<CommentFetchResponse>(GET_COMMENTS).then((res) => res.data);
+
+export const getCommentsPagination = (
+  pageParam: number
+): Promise<CommentFetchResponse> =>
+  axios
+    .post<CommentFetchResponse>(GET_COMMENTS_PAGINATION, { pageParam })
+    .then((res) => res.data);
 
 export const createComment = (
   reqBody: CreateCommentFormInputs
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,2 +1,3 @@
 export const GET_COMMENTS = "/getComments";
+export const GET_COMMENTS_PAGINATION = "/getCommentsPagination";
 export const CREATE_COMMENT = "/createComment";
